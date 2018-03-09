a293aed1ed47e91a7252fe5d4f80131a18c8f466
nyyirs <36409640+nyyirs@users.noreply.github.com>
2018-03-09 11:51:21 +0400

    new pages

--- server.js
+++ server.js
@@ -7,10 +7,67 @@ var app = express();
 
 app.set('view engine', 'hbs');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
+
+    res.render('index.hbs');
+
+});
+
+app.get('/mcine', (req, res) => {
     let titles = [];
     let links = [];
     let imgs = [];
+    var movies = [];
+    let url = 'https://www.myt.mu/sinformer/cinema/les-salles/23/cinema-mcine';
+
+    request(url, (error, response, body) => {
+
+        var $ = cheerio.load(body);
+
+        //Query Movie Titles
+        $('.col-md-8').filter(function () {
+            var data = $(this);
+            title = data.children().first().text();
+            titles.push(title);
+        });
+
+        //Query Movie Links
+        $('.col-md-4').filter(function () {
+            var data = $(this);
+            link = data.children().attr('href');
+            links.push(`https://www.myt.mu${link}`);
+        });
+
+        //Query Movie Links
+        $('.col-md-4').filter(function () {
+            var data = $('a', this);
+            img = data.children().first().attr('src');
+            imgs.push(img);
+
+        });
+
+        
+
+        for (var i = 0; i < imgs.length; i++) {
+
+            movies[i] = { 'image': imgs[i], 'link': links[i], 'title': titles[i]};
+
+        };
+
+        res.render('mcine.hbs', { movies });
+
+    });
+
+   
+});
+
+app.get('/bagatelle', (req, res) => {
+    let titles = [];
+    let links = [];
+    let imgs = [];
+    var movies = [];
     let url = 'https://www.myt.mu/sinformer/cinema/les-salles/19/cinema-star-bagatelle';
 
     request(url, (error, response, body) => {
@@ -39,17 +96,19 @@ app.get('/', (req, res) => {
 
         });
 
-        var movies = []; 
         
-        for (var i = 0; i < imgs.length; i ++) { 
-            
-            movies[i] = { 'image': imgs[i], 'link': links[i], 'title': titles[i] } 
-        
+
+        for (var i = 0; i < imgs.length; i++) {
+
+            movies[i] = { 'image': imgs[i], 'link': links[i], 'title': titles[i]};
+
         };
 
-        res.render('index.hbs', { movies });
+        res.render('bagatelle.hbs', { movies });
 
     });
+
+   
 });
 
 //Run the Server
